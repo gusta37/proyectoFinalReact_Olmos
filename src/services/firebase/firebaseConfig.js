4a48e8c3073a1a95efe341fe3d367b777a2d2452
Gustavo olmos <grolmos159@gmail.com>
2023-11-26 01:21:25 -0300

    return to firebase

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,12 +2,19 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+
+    apiKey: "AIzaSyAupRMNm2JHD4iosuyI_XJO5IseZWfsg_A",
+  
+    authDomain: "ecommerce-proyecto-final-4c4b3.firebaseapp.com",
+  
+    projectId: "ecommerce-proyecto-final-4c4b3",
+  
+    storageBucket: "ecommerce-proyecto-final-4c4b3.appspot.com",
+  
+    messagingSenderId: "177236506122",
+  
+    appId: "1:177236506122:web:e33b93101e1bb9267251f3"
+  
 };
 
 const app = initializeApp(firebaseConfig);
